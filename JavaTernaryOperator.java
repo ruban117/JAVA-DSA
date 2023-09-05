ca199a40770cccd14cc3eec16390ffbd73685187
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-09-05 11:41:35 +0530

    Ternary Operator

---
+++ JavaTernaryOperator.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class JavaTernaryOperator {
+    public static void main(String[] args) {
+        String type=(5%2 == 0)? "Even" : "Odd";
+        System.out.println("The Number Is:- "+type);
+    }
+}
