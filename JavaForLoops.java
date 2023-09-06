068bdf29fd4b9e3913a2d079ed7727c5438d2edd
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-09-06 21:12:55 +0530

    Add files via upload

---
+++ JavaForLoops.java
@@ -0,0 +1,22 @@
+import  java.util.Scanner;
+public class JavaForLoops {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        for(int i=1;i<=3;i++){
+            System.out.println("Hello");
+        }
+
+        /*Square Pattern
+         ****
+         ****
+         ****
+         ****
+         */
+
+        for(int i=1;i<=4;i++){
+            System.out.println("* * * *");
+        }
+
+        
+    }
+}
