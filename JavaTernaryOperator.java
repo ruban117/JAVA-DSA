25c7f8398e7448721740c47b700246bce98dd60e
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-09-06 19:58:43 +0530

    Update JavaTernaryOperator.java

--- JavaTernaryOperator.java
+++ JavaTernaryOperator.java
@@ -3,5 +3,11 @@ public class JavaTernaryOperator {
     public static void main(String[] args) {
         String type=(5%2 == 0)? "Even" : "Odd";
         System.out.println("The Number Is:- "+type);
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Marks:- ");
+        int a=sc.nextInt();
+        String res=(a > 33)? "Pass" : "Fail";
+        System.out.println(res);
     }
 }
