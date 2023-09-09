2e3d1bd97e1827e36cc61efdd6b7a58fd140e074
Ruban <rubanpathak706@gmail.com>
2023-09-09 12:30:02 +0530

    Function In Java

---
+++ JavaFunction.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class JavaFunction{
+    public static void printhelloworld(){
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        printhelloworld();
+    }
+}
